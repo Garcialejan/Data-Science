190ee53279d38951d6286ae5a5d076ae29bd912f
Alejandro_pc\Alejandro <garcialejan@gmail.com>
2025-02-25 19:37:49 +0100

    Fixing problems before deployment on AWS

--- 01_data_science_theory/04_machine_learning/ML_project_implementation/fastapi_aplication.py
+++ 01_data_science_theory/04_machine_learning/ML_project_implementation/fastapi_aplication.py
@@ -1,4 +1,9 @@
 import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from typing import Optional
+
 from fastapi import FastAPI, Request, HTTPException, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -13,7 +18,6 @@ path_scaler = "./models/scaler.pkl"
 try:
     with open(path_model, "rb") as file:
         model = pickle.load(file)
-    
     with open(path_scaler, "rb") as file:
         scaler = pickle.load(file)
 except Exception as e:
@@ -36,38 +40,22 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html",
                                       {"request":request})
 
-# Build the prediction
+
 @app.get("/predictdata", response_class=HTMLResponse)
+async def predict_datapoint_get(request: Request):
+    return templates.TemplateResponse('home.html', {"request": request})
+
+# Build the prediction
+#* Form es una clase que se utiliza para extraer datos de formularios 
+#* HTML enviados mediante solicitudes POST
 @app.post("/predictdata", response_class=HTMLResponse)
-async def predict_datapoint(request:Request,
-                            Temperature: float = Form(...),
-                            RH: float = Form(...),
-                            Ws: float = Form(...),
-                            Rain: float = Form(...),
-                            FFMC: float = Form(...),
-                            DMC: float = Form(...),
-                            ISI: float = Form(...),
-                            Classes: float = Form(...),
-                            Region: float = Form(...)):
-    if request.method == "POST":
-        # Crea un diccionario con los datos de entrada
-        input_data = {
-            "Temperature": Temperature,
-            "RH": RH,
-            "Ws": Ws,
-            "Rain": Rain,
-            "FFMC": FFMC,
-            "DMC": DMC,
-            "ISI": ISI,
-            "Classes": Classes,
-            "Region": Region
-        }
-        input_list = [input_data[key] for key in input_data]
-        new_data_scaled=scaler.transform([input_list])
-        result=model.predict(new_data_scaled)[0]
+async def predict_datapoint_post(request: Request,
+                                 input_data: InputData):
+    try:
+        input_list = list(input_data.model_dump().values())
+        new_data_scaled = scaler.transform([input_list])
+        result = model.predict(new_data_scaled)[0]
         
-        return templates.TemplateResponse('home.html',
-                                          {"request": request, "result": result})
-    else:
-        return templates.TemplateResponse('home.html',
-                                          {"request": request})
+        return templates.TemplateResponse('home.html', {"request": request, "result": result})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error during prediction: {str(e)}")
